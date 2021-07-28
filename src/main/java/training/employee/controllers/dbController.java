74a06df835df9f9049a81bd3c5e416264a107e64
VladimirKalachev <arrrrgh@inbox.ru>
2021-07-28 11:51:45 +0300

    modify application.properties, build.gradle, create UserRepository interface

---
+++ src/main/java/training/employee/controllers/dbController.java
@@ -0,0 +1,26 @@
+package training.employee.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import training.employee.models.Users;
+import training.employee.repo.UserRepository;
+
+@Controller
+public class dbController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/")
+    public String userAdd(@RequestParam String first_name,
+                          @RequestParam String last_name, @RequestParam int company_id,
+                          @RequestParam String role, Model model){
+        Users users = new Users(first_name, last_name, company_id, role);
+        userRepository.save(users);
+        return "/message";
+    }
+
+}
