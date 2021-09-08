1a26f12d2e2fd006ccf8d460c39687c1d69dc650
VladimirKalachev <arrrrgh@inbox.ru>
2021-09-08 09:00:11 +0300

    create UserService.java refactor UserController.java

--- src/main/java/com/employee/controllers/UserController.java
+++ src/main/java/com/employee/controllers/UserController.java
@@ -1,35 +1,15 @@
 package com.employee.controllers;
 
+import com.employee.controllers.service.UserService;
 import com.employee.repo.UserRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import com.employee.models.Users;
 
 
 @Controller
-public class UserController {
-
-    private final UserRepository userRepository;
+public class UserController extends UserService {
 
     public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+        super(userRepository);
     }
 
-    @PostMapping("/main")
-    @Transactional
-    public String userAdd(@RequestParam(value = "firstname", required = false) String firstName,
-                          @RequestParam(value = "lastname", required = false)  String lastName,
-                          @RequestParam(value = "companyid", required = false) int companyId,
-                          @RequestParam(value = "role", required = false) String role,
-                          Model model){
-
-        Users users = new Users(firstName, lastName, companyId, role);
-
-        userRepository.save(users);
-
-        return "/message";
-    }
 }
