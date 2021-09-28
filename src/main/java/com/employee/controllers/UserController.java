7d21471aeb8df042f264fa5ddea02943986d1d45
VladimirKalachev <arrrrgh@inbox.ru>
2021-09-28 13:45:04 +0300

    add upload csv file function

--- src/main/java/com/employee/controllers/UserController.java
+++ src/main/java/com/employee/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class UserController {
@@ -20,12 +21,12 @@ public class UserController {
 
     @PostMapping("/main")
     public String createUser(@RequestParam(value = "firstname", required = false) String firstName,
-                         @RequestParam(value = "lastname", required = false)  String lastName,
-                         @RequestParam(value = "companyid", required = false) int companyId,
-                         @RequestParam(value = "role", required = false) String role,
-                         Model model) {
+                             @RequestParam(value = "lastname", required = false) String lastName,
+                             @RequestParam(value = "companyid", required = false) int companyId,
+                             @RequestParam(value = "role", required = false) String role,
+                             Model model) {
 
-        Long userID =  userService.addUser(firstName, lastName, companyId, role).getId();
+        Long userID = userService.addUser(firstName, lastName, companyId, role).getId();
         model.addAttribute("userID", userID);
         return "/main";
     }
@@ -33,21 +34,21 @@ public class UserController {
     @GetMapping("/list")
     public String listData(Model model) {
 
-        Iterable users  = userService.userList();
+        Iterable users = userService.userList();
         model.addAttribute("userList", users);
         return "/list";
     }
 
     @GetMapping("/delete/{id}")
     public String deleteUser(@PathVariable(value = "id", required = false) Long id,
-                             Model model){
+                             Model model) {
         userService.deleteUser(id);
         return "redirect:/list";
     }
 
     @GetMapping("/edit/{id}")
     public String editUser(@PathVariable(value = "id") Long id,
-                           Model model){
+                           Model model) {
 
         model.addAttribute("user", userService.editUser(id));
         return "/edit";
@@ -56,24 +57,43 @@ public class UserController {
 
     @PostMapping("/edit/{id}")
     public String updUser(@PathVariable(value = "id", required = false) Long id,
-                           @RequestParam(value = "firstname", required = false) String firstName,
-                           @RequestParam(value = "lastname", required = false)  String lastName,
-                           @RequestParam(value = "companyid", required = false) Integer companyId,
-                           @RequestParam(value = "role", required = false) String role,
-                           Model model) {
+                          @RequestParam(value = "firstname", required = false) String firstName,
+                          @RequestParam(value = "lastname", required = false) String lastName,
+                          @RequestParam(value = "companyid", required = false) Integer companyId,
+                          @RequestParam(value = "role", required = false) String role,
+                          Model model) {
 
-        User user =  userService.updateUser(id, firstName, lastName, companyId, role);
+        User user = userService.updateUser(id, firstName, lastName, companyId, role);
         model.addAttribute("user", user);
 
         return "/edit";
     }
 
+    @PostMapping("/csv-upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file,
+                             Model model) {
+        System.out.println("Upload was pressed");
+        if (file.isEmpty()) {
+            model.addAttribute("message", "Please select a CSV file to upload");
+            //model.addAttribute("status", false);
+            System.out.println("file is EMPTY");
+        } else {
 
+            System.out.println("File NOT empty. Call service");
 
+                userService.csvToUsers(file);
+            }
 
+        return "/main";
 
-
-
-
-
+    }
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/com/employee/service/UserService.java
+++ src/main/java/com/employee/service/UserService.java
@@ -2,8 +2,16 @@ package com.employee.service;
 
 import com.employee.models.User;
 import com.employee.repo.UserRepository;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -55,8 +63,28 @@ public class UserService {
         user.setRole(role);
 
         return userRepository.save(user);
-
     }
+
+    @Transactional
+    public void csvToUsers(MultipartFile file) {
+        System.out.println("Service called");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))){
+            CsvToBean csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(User.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            List users = csvToBean.parse();
+
+            System.out.println(users);
+
+                userRepository.saveAll(users);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
 
 
